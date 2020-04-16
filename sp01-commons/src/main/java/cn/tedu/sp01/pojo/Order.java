c3d175c7e193fa4ab6c6453e0f26bf1f2d3334cf
asus <asus@DESKTOP-KMNT0DI>
2020-04-16 18:46:09 +0800

    111

---
+++ sp01-commons/src/main/java/cn/tedu/sp01/pojo/Order.java
@@ -0,0 +1,43 @@
+package cn.tedu.sp01.pojo;
+
+import java.util.List;
+
+public class Order {
+	private String id;
+	private User user;
+	private List<Item> items;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public List<Item> getItems() {
+		return items;
+	}
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+	public Order() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Order(String id, User user, List<Item> items) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.items = items;
+	}
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", user=" + user + ", items=" + items + "]";
+	}
+	
+	
+}
